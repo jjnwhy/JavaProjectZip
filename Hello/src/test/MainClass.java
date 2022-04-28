6a47b505f9734d0903382a2141fab0846b42a222
jjnwhy <comsa09@naver.com>
2022-04-28 17:01:38 +0900

    Hello 프로젝트

---
+++ Hello/src/test/MainClass.java
@@ -0,0 +1,14 @@
+package test;
+
+public class MainClass {
+	// run(실행)했을때 실행의 흐름이 시작되는 main메소드
+	public static void main(String[] args) {
+
+		System.out.println("HelloWorld!");
+
+		String str = "gura";
+		int num1 = 10;
+		boolean isRun = true;
+
+	}
+}
